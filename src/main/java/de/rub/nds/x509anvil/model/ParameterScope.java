c003bb62560fdb18b1a42ef2a2f5be6c47b6ae28
Jonas <jonasthi97@gmail.com>
2022-06-14 15:16:38 +0200

    Add ParameterIdentifier

---
+++ src/main/java/de/rub/nds/x509anvil/model/ParameterScope.java
@@ -0,0 +1,8 @@
+package de.rub.nds.x509anvil.model;
+
+public enum ParameterScope {
+    GLOBAL,
+    ENTITY_CERT,
+    INTERMEDIATE_CERT,
+    ROOT_CERT
+}
---
+++ src/main/java/de/rub/nds/x509anvil/model/ParameterType.java
@@ -0,0 +1,6 @@
+package de.rub.nds.x509anvil.model;
+
+public enum ParameterType {
+    CHAIN_LENGTH,
+    VERSION
+}
