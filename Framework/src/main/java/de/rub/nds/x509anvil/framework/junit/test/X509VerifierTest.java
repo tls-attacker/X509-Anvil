bb6557940180a4d7008f3819f2b84d1f5c020141
Jonas <jonasthi97@gmail.com>
2022-06-23 09:03:51 +0200

    Move static calls to JUnit test class so that they are called when running individual test cases too

--- Framework/src/main/java/de/rub/nds/x509anvil/framework/junit/test/X509VerifierTest.java
+++ Framework/src/main/java/de/rub/nds/x509anvil/framework/junit/test/X509VerifierTest.java
@@ -1,18 +1,22 @@
 package de.rub.nds.x509anvil.framework.junit.test;
 
-import de.rub.nds.x509anvil.framework.TestContext;
-import de.rub.nds.x509anvil.framework.junit.execution.X509TestRunner;
+import de.rub.nds.x509anvil.framework.junit.context.TestContext;
+import de.rub.nds.x509anvil.framework.junit.execution.X509VerifierRunner;
 import de.rub.nds.x509anvil.framework.junit.extension.ExtensionContextParameterResolver;
 import de.rub.nds.x509anvil.framework.junit.extension.X509TestRunnerResolver;
 import de.rub.nds.x509anvil.framework.model.DerivationScope;
 import de.rub.nds.x509anvil.framework.model.ParameterCombination;
 import de.rub.nds.x509anvil.framework.x509.config.X509CertificateChainConfig;
+import de.rub.nds.x509attacker.registry.Registry;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+
+import java.security.Security;
 
 
 @ExtendWith({
@@ -29,6 +33,12 @@ import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 public abstract class X509VerifierTest {
     protected static final Logger LOGGER = LogManager.getLogger();
 
+    static {
+        // We need to call this statically, otherwise we would not be able to generate certificates from unit tests
+        Security.addProvider(new BouncyCastleProvider());
+        Registry.getInstance();
+    }
+
     protected TestContext testContext;
     protected ExtensionContext extensionContext;
     protected ParameterCombination parameterCombination;
@@ -42,7 +52,7 @@ public abstract class X509VerifierTest {
         this.extensionContext = extensionContext;
     }
 
-    public X509CertificateChainConfig prepareConfig(ArgumentsAccessor argumentsAccessor, X509TestRunner testRunner) {
+    public X509CertificateChainConfig prepareConfig(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) {
         X509CertificateChainConfig config = initConfig();
         parameterCombination = ParameterCombination.fromArgumentsAccessor(argumentsAccessor, new DerivationScope(extensionContext));
         parameterCombination.applyToConfig(config, testContext);
