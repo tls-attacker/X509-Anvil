c89ea82e39c1792da1b437af05cc5cfd3a875cb9
Jonas <jonasthi97@gmail.com>
2022-07-13 16:01:53 +0200

    Register parameter types and factory

--- Framework/src/main/java/Main.java
+++ Framework/src/main/java/Main.java
@@ -7,46 +7,13 @@
  * Licensed under Apache License, Version 2.0
  * http://www.apache.org/licenses/LICENSE-2.0.txt
  */
-import de.rub.nds.asn1.Asn1Encodable;
-import de.rub.nds.asn1.model.*;
-import de.rub.nds.asn1.parser.Asn1Parser;
-import de.rub.nds.asn1.parser.IntermediateAsn1Field;
-import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
-import de.rub.nds.x509anvil.framework.model.ParameterIdentifier;
-import de.rub.nds.x509anvil.framework.model.ParameterScope;
-import de.rub.nds.x509anvil.framework.model.ParameterType;
-import de.rub.nds.x509anvil.framework.verifier.TlsClientAuthVerifierAdapter;
-import de.rub.nds.x509anvil.framework.verifier.VerifierException;
-import de.rub.nds.x509anvil.framework.verifier.VerifierResult;
-import de.rub.nds.x509anvil.framework.x509.config.X509CertificateChainConfig;
-import de.rub.nds.x509anvil.framework.x509.config.X509Util;
-import de.rub.nds.x509anvil.framework.x509.config.constants.AlgorithmObjectIdentifiers;
-import de.rub.nds.x509anvil.framework.x509.config.X509CertificateConfig;
-import de.rub.nds.x509anvil.framework.x509.config.constants.AttributeTypeObjectIdentifiers;
-import de.rub.nds.x509anvil.framework.x509.config.model.*;
-import de.rub.nds.x509anvil.framework.x509.config.model.Signer;
-import de.rub.nds.x509anvil.framework.x509.generator.X509CertificateChainGenerator;
-import de.rub.nds.x509attacker.registry.Registry;
-import de.rub.nds.x509attacker.x509.X509Certificate;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
-import java.io.IOException;
-import java.math.BigInteger;
-import java.security.*;
-import java.util.List;
+import de.rub.nds.anvilcore.context.AnvilContext;
+import de.rub.nds.x509anvil.framework.anvil.X509AnvilParameterFactory;
+import de.rub.nds.x509anvil.framework.anvil.X509AnvilParameterType;
 
 public class Main {
-    public static void main(String[] args)
-        throws NoSuchAlgorithmException, CertificateGeneratorException, IOException, VerifierException {
-        Security.addProvider(new BouncyCastleProvider());
-        Registry.getInstance();
-
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-        keyPairGenerator.initialize(4096);
-        KeyPair keyPair = keyPairGenerator.generateKeyPair();
-
-        Asn1Parser asn1Parser = new Asn1Parser(keyPair.getPublic().getEncoded(), true);
-        List<IntermediateAsn1Field> encodables = asn1Parser.parseIntermediateFields();
-        System.out.println();
+    public static void main(String[] args) {
+        AnvilContext.getInstance().addParameterTypes(X509AnvilParameterType.values(), new X509AnvilParameterFactory());
     }
 }
