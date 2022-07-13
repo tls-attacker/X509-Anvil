fba1ef28072615479fef938a385b85a9a67ff239
Jonas <jonasthi97@gmail.com>
2022-07-13 16:16:43 +0200

    Add preliminary parameter scope implementation

---
+++ Framework/src/main/java/de/rub/nds/x509anvil/framework/anvil/X509AnvilParameterScope.java
@@ -0,0 +1,11 @@
+package de.rub.nds.x509anvil.framework.anvil;
+
+import de.rub.nds.anvilcore.model.parameter.ParameterScope;
+
+public class X509AnvilParameterScope extends ParameterScope {
+    // TODO Implement scope behavior
+    @Override
+    public String getUniqueScopeIdentifier() {
+        return "to be implemented";
+    }
+}
