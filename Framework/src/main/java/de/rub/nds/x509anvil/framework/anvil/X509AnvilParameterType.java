eb0780e5ac6675e590d0f1dcf5a78155c99a0307
Jonas <jonasthi97@gmail.com>
2022-07-13 15:50:45 +0200

    Implement ParameterType

---
+++ Framework/src/main/java/de/rub/nds/x509anvil/framework/anvil/X509AnvilParameterType.java
@@ -0,0 +1,8 @@
+package de.rub.nds.x509anvil.framework.anvil;
+
+import de.rub.nds.anvilcore.model.parameter.ParameterType;
+
+public enum X509AnvilParameterType implements ParameterType {
+    CHAIN_LENGTH,
+    VERSION
+}
