1c37162a494930a900058393ef25ea3003b05cee
Jonas <jonasthi97@gmail.com>
2022-08-17 18:04:45 +0200

    Refactor validity tests

--- X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/basicfields/ValidityTests.java
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/basicfields/ValidityTests.java
@@ -4,7 +4,6 @@ package de.rub.nds.x509anvil.suite.tests.basicfields;
 import de.rub.nds.anvilcore.annotation.AnvilTest;
 import de.rub.nds.anvilcore.annotation.IpmLimitations;
 import de.rub.nds.anvilcore.annotation.TestStrength;
-import de.rub.nds.asn1.model.Asn1Integer;
 import de.rub.nds.x509anvil.framework.annotation.ChainLength;
 import de.rub.nds.x509anvil.framework.annotation.RFC;
 import de.rub.nds.x509anvil.framework.annotation.SeverityLevel;
@@ -14,22 +13,19 @@ import de.rub.nds.x509anvil.framework.constants.Severity;
 import de.rub.nds.x509anvil.framework.verifier.VerifierException;
 import de.rub.nds.x509anvil.framework.verifier.VerifierResult;
 import de.rub.nds.x509anvil.framework.x509.config.X509CertificateChainConfig;
-import de.rub.nds.x509anvil.framework.x509.config.X509Util;
 import de.rub.nds.x509anvil.framework.x509.config.model.TimeType;
 import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 
-import java.math.BigInteger;
-
 public class ValidityTests extends X509AnvilTest {
 
-    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing")
-    @AnvilTest(description = "The certificate validity period includes the current time.")
+    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing", text = "The certificate validity period includes the current time.")
     @SeverityLevel(Severity.CRITICAL)
     @ChainLength(minLength = 2, maxLength = 3, intermediateCertsModeled = 2)
     @TestStrength(2)
     @IpmLimitations(identifiers = "entity.not_before")
+    @AnvilTest
     public void entityCertNotYetValidUtc(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
         X509CertificateChainConfig chainConfig = prepareConfig(argumentsAccessor, testRunner);
         chainConfig.getEntityCertificateConfig().setNotBeforeValue("400101000000Z");
@@ -38,12 +34,12 @@ public class ValidityTests extends X509AnvilTest {
         Assertions.assertFalse(result.isValid());
     }
 
-    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing")
-    @AnvilTest(description = "The certificate validity period includes the current time.")
+    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing", text = "The certificate validity period includes the current time.")
     @SeverityLevel(Severity.CRITICAL)
     @ChainLength(minLength = 2, maxLength = 3, intermediateCertsModeled = 2)
     @TestStrength(2)
     @IpmLimitations(identifiers = "entity.not_before")
+    @AnvilTest
     public void entityCertNotYetValidGeneralized(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
         X509CertificateChainConfig chainConfig = prepareConfig(argumentsAccessor, testRunner);
         chainConfig.getEntityCertificateConfig().setNotBeforeValue("20400101000000Z");
@@ -52,12 +48,12 @@ public class ValidityTests extends X509AnvilTest {
         Assertions.assertFalse(result.isValid());
     }
 
-    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing")
-    @AnvilTest(description = "The certificate validity period includes the current time.")
+    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing", text = "The certificate validity period includes the current time.")
     @SeverityLevel(Severity.CRITICAL)
     @ChainLength(minLength = 3, maxLength = 3, intermediateCertsModeled = 2)
     @TestStrength(2)
     @IpmLimitations(identifiers = "inter0.not_before")
+    @AnvilTest
     public void intermediateCertNotYetValidUtc(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
         X509CertificateChainConfig chainConfig = prepareConfig(argumentsAccessor, testRunner);
         chainConfig.getIntermediateConfig(0).setNotBeforeValue("400101000000Z");
@@ -66,12 +62,12 @@ public class ValidityTests extends X509AnvilTest {
         Assertions.assertFalse(result.isValid());
     }
 
-    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing")
-    @AnvilTest(description = "The certificate validity period includes the current time.")
+    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing", text = "The certificate validity period includes the current time.")
     @SeverityLevel(Severity.CRITICAL)
     @ChainLength(minLength = 3, maxLength = 3, intermediateCertsModeled = 2)
     @TestStrength(2)
     @IpmLimitations(identifiers = "inter0.not_before")
+    @AnvilTest
     public void intermediateCertNotYetValidGeneralized(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
         X509CertificateChainConfig chainConfig = prepareConfig(argumentsAccessor, testRunner);
         chainConfig.getIntermediateConfig(0).setNotBeforeValue("20400101000000Z");
@@ -80,12 +76,12 @@ public class ValidityTests extends X509AnvilTest {
         Assertions.assertFalse(result.isValid());
     }
 
-    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing")
-    @AnvilTest(description = "The certificate validity period includes the current time.")
+    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing", text = "The certificate validity period includes the current time.")
     @SeverityLevel(Severity.CRITICAL)
     @ChainLength(minLength = 2, maxLength = 3, intermediateCertsModeled = 2)
     @TestStrength(2)
     @IpmLimitations(identifiers = "entity.not_after")
+    @AnvilTest
     public void entityCertExpiredUtc(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
         X509CertificateChainConfig chainConfig = prepareConfig(argumentsAccessor, testRunner);
         chainConfig.getEntityCertificateConfig().setNotAfterValue("200101000000Z");
@@ -94,12 +90,12 @@ public class ValidityTests extends X509AnvilTest {
         Assertions.assertFalse(result.isValid());
     }
 
-    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing")
-    @AnvilTest(description = "The certificate validity period includes the current time.")
+    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing", text = "The certificate validity period includes the current time.")
     @SeverityLevel(Severity.CRITICAL)
     @ChainLength(minLength = 2, maxLength = 3, intermediateCertsModeled = 2)
     @TestStrength(2)
     @IpmLimitations(identifiers = "entity.not_after")
+    @AnvilTest
     public void entityCertExpiredGeneralized(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
         X509CertificateChainConfig chainConfig = prepareConfig(argumentsAccessor, testRunner);
         chainConfig.getEntityCertificateConfig().setNotAfterValue("20200101000000Z");
@@ -108,12 +104,12 @@ public class ValidityTests extends X509AnvilTest {
         Assertions.assertFalse(result.isValid());
     }
 
-    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing")
-    @AnvilTest(description = "The certificate validity period includes the current time.")
+    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing", text = "The certificate validity period includes the current time.")
     @SeverityLevel(Severity.CRITICAL)
     @ChainLength(minLength = 3, maxLength = 3, intermediateCertsModeled = 2)
     @TestStrength(2)
     @IpmLimitations(identifiers = "inter0.not_after")
+    @AnvilTest
     public void intermediateCertExpiredUtc(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
         X509CertificateChainConfig chainConfig = prepareConfig(argumentsAccessor, testRunner);
         chainConfig.getIntermediateConfig(0).setNotAfterValue("200101000000Z");
@@ -122,12 +118,12 @@ public class ValidityTests extends X509AnvilTest {
         Assertions.assertFalse(result.isValid());
     }
 
-    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing")
-    @AnvilTest(description = "The certificate validity period includes the current time.")
+    @RFC(number = 5280, section = "6.1.3. Basic Certificate Processing", text = "The certificate validity period includes the current time.")
     @SeverityLevel(Severity.CRITICAL)
     @ChainLength(minLength = 3, maxLength = 3, intermediateCertsModeled = 2)
     @TestStrength(2)
     @IpmLimitations(identifiers = "inter0.not_after")
+    @AnvilTest
     public void intermediateCertExpiredGeneralized(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
         X509CertificateChainConfig chainConfig = prepareConfig(argumentsAccessor, testRunner);
         chainConfig.getIntermediateConfig(0).setNotAfterValue("20200101000000Z");
