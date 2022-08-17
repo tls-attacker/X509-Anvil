17e1c4c3291a744726f67e960d6766e89e8cb856
Jonas <jonasthi97@gmail.com>
2022-08-17 18:22:11 +0200

    Add test for chains with untrusted root certificate

---
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/common/UntrustedRootTests.java
@@ -0,0 +1,29 @@
+package de.rub.nds.x509anvil.suite.tests.common;
+
+import de.rub.nds.anvilcore.annotation.AnvilTest;
+import de.rub.nds.anvilcore.annotation.TestStrength;
+import de.rub.nds.x509anvil.framework.annotation.ChainLength;
+import de.rub.nds.x509anvil.framework.annotation.RFC;
+import de.rub.nds.x509anvil.framework.annotation.StaticRoot;
+import de.rub.nds.x509anvil.framework.anvil.X509AnvilTest;
+import de.rub.nds.x509anvil.framework.anvil.X509VerifierRunner;
+import de.rub.nds.x509anvil.framework.verifier.VerifierException;
+import de.rub.nds.x509anvil.framework.verifier.VerifierResult;
+import de.rub.nds.x509anvil.framework.x509.config.X509CertificateChainConfig;
+import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+
+public class UntrustedRootTests extends X509AnvilTest {
+
+    @RFC(number = 5280)
+    @AnvilTest(description = "Checks whether the verifying application rejects a chain when the root certificate is untrusted")
+    @StaticRoot(false)
+    @ChainLength(minLength = 1, maxLength = 2, intermediateCertsModeled = 2)
+    @TestStrength(2)
+    public void untrustedRootCertificate(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        X509CertificateChainConfig config = prepareConfig(argumentsAccessor, testRunner);
+        VerifierResult result = testRunner.execute(config);
+        Assertions.assertFalse(result.isValid());
+    }
+}
