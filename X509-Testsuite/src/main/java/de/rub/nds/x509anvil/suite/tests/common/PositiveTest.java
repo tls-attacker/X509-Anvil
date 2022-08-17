b3ba246a53d57f7fe592bd03a5e272df105f1805
Jonas <jonasthi97@gmail.com>
2022-08-17 18:03:30 +0200

    Add positive test template

---
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/common/PositiveTest.java
@@ -0,0 +1,25 @@
+package de.rub.nds.x509anvil.suite.tests.common;
+
+import de.rub.nds.anvilcore.annotation.AnvilTest;
+import de.rub.nds.anvilcore.annotation.TestStrength;
+import de.rub.nds.x509anvil.framework.annotation.ChainLength;
+import de.rub.nds.x509anvil.framework.anvil.X509AnvilTest;
+import de.rub.nds.x509anvil.framework.anvil.X509VerifierRunner;
+import de.rub.nds.x509anvil.framework.verifier.VerifierException;
+import de.rub.nds.x509anvil.framework.verifier.VerifierResult;
+import de.rub.nds.x509anvil.framework.x509.config.X509CertificateChainConfig;
+import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+
+public class PositiveTest extends X509AnvilTest {
+
+    @AnvilTest(description = "Positive test that covers all combinations provided by our model")
+    @ChainLength(minLength = 2, maxLength = 4, intermediateCertsModeled = 2)
+    @TestStrength(2)
+    public void sampleTestCase(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        X509CertificateChainConfig config = prepareConfig(argumentsAccessor, testRunner);
+        VerifierResult result = testRunner.execute(config);
+        Assertions.assertTrue(result.isValid());
+    }
+}
