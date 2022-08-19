5fe7a4ef5af1c59d4f2e9b507bd3e96fc0bcd067
Jonas <jonasthi97@gmail.com>
2022-08-19 17:33:52 +0200

    Add tests for unique identifiers

---
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/util/Constraints.java
@@ -0,0 +1,11 @@
+package de.rub.nds.x509anvil.suite.tests.util;
+
+public class Constraints {
+    public static boolean disabled(Boolean value) {
+        return !value;
+    }
+
+    public static boolean allowVersion1(Integer value) {
+        return value.equals(0);
+    }
+}
