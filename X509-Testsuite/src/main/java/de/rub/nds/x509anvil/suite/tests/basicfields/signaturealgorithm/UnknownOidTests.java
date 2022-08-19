a0ef856dffb4b0ac4804c7ffcb773d22414c8fc2
Jonas <jonasthi97@gmail.com>
2022-08-19 22:16:24 +0200

    Refactor signatureAlgorithm tests

--- X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/basicfields/signaturealgorithm/UnknownOidTests.java
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/basicfields/signaturealgorithm/UnknownOidTests.java
@@ -1,4 +1,4 @@
-package de.rub.nds.x509anvil.suite.tests.basicfields;
+package de.rub.nds.x509anvil.suite.tests.basicfields.signaturealgorithm;
 
 import de.rub.nds.anvilcore.annotation.AnvilTest;
 import de.rub.nds.anvilcore.annotation.TestStrength;
@@ -15,54 +15,14 @@ import de.rub.nds.x509anvil.framework.x509.config.X509CertificateChainConfig;
 import de.rub.nds.x509anvil.framework.x509.config.X509Util;
 import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
 import de.rub.nds.x509anvil.framework.x509.generator.X509CertificateChainGenerator;
-import de.rub.nds.x509anvil.suite.tests.util.TestUtils;
+import de.rub.nds.x509anvil.suite.tests.basicfields.TbsSignatureTests;
 import de.rub.nds.x509attacker.x509.X509Certificate;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 
 import java.util.List;
 
-public class SignatureAlgorithmTests extends X509AnvilTest {
-
-    @RFC(number = 5280, section = "4.1.1.2. signatureAlgorithm",
-            text = "This field MUST contain the same algorithm identifier as the signature field in the sequence tbsCertificate (Section 4.1.2.3).")
-    @SeverityLevel(Severity.ERROR)
-    @ChainLength(minLength = 2, maxLength = 3, intermediateCertsModeled = 2)
-    @TestStrength(2)
-    @AnvilTest
-    public void signatureAlgorithmFieldDoesntMatchAlgorithmEntity(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
-        X509CertificateChainConfig certificateChainConfig = prepareConfig(argumentsAccessor, testRunner);
-        X509CertificateChainGenerator certificateChainGenerator = new X509CertificateChainGenerator(certificateChainConfig);
-        certificateChainGenerator.generateCertificateChain();
-        List<X509Certificate> generatedCertificates = certificateChainGenerator.retrieveCertificateChain();
-        Asn1ObjectIdentifier signatureAlgorithmAsn1 = (Asn1ObjectIdentifier) X509Util.getAsn1ElementByIdentifierPath(
-                generatedCertificates.get(generatedCertificates.size()-1),
-                "signatureAlgorithm", "algorithm");
-        signatureAlgorithmAsn1.setValue(TestUtils.getNonMatchingAlgorithmOid(certificateChainConfig.getIssuerConfigOf(
-                certificateChainConfig.getEntityCertificateConfig()).getSignatureAlgorithm()));
-        VerifierResult result = testRunner.execute(generatedCertificates, certificateChainConfig);
-        Assertions.assertFalse(result.isValid());
-    }
-
-    @RFC(number = 5280, section = "4.1.1.2. signatureAlgorithm",
-            text = "This field MUST contain the same algorithm identifier as the signature field in the sequence tbsCertificate (Section 4.1.2.3).")
-    @SeverityLevel(Severity.ERROR)
-    @ChainLength(minLength = 3, maxLength = 3, intermediateCertsModeled = 2)
-    @TestStrength(2)
-    @AnvilTest
-    public void signatureAlgorithmFieldDoesntMatchAlgorithmIntermediate(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
-        X509CertificateChainConfig certificateChainConfig = prepareConfig(argumentsAccessor, testRunner);
-        X509CertificateChainGenerator certificateChainGenerator = new X509CertificateChainGenerator(certificateChainConfig);
-        certificateChainGenerator.generateCertificateChain();
-        List<X509Certificate> generatedCertificates = certificateChainGenerator.retrieveCertificateChain();
-        Asn1ObjectIdentifier signatureAlgorithmAsn1 = (Asn1ObjectIdentifier) X509Util.getAsn1ElementByIdentifierPath(
-                generatedCertificates.get(generatedCertificates.size()-2),
-                "signatureAlgorithm", "algorithm");
-        signatureAlgorithmAsn1.setValue(TestUtils.getNonMatchingAlgorithmOid(certificateChainConfig.getIssuerConfigOf(
-                certificateChainConfig.getIntermediateConfig(0)).getSignatureAlgorithm()));
-        VerifierResult result = testRunner.execute(generatedCertificates, certificateChainConfig);
-        Assertions.assertFalse(result.isValid());
-    }
+public class UnknownOidTests extends X509AnvilTest {
 
     @RFC(number = 5280, section = "4.1.1.2. signatureAlgorithm and 4.1.2.3")
     @SeverityLevel(Severity.ERROR)
