9519e8590ac3da22b1f364555af0c78aed0c84e9
Jonas <jonasthi97@gmail.com>
2022-08-19 22:09:10 +0200

    Refactor serialnumber tests

---
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/basicfields/serialnumber/ZeroSerialNumberTests.java
@@ -0,0 +1,53 @@
+package de.rub.nds.x509anvil.suite.tests.basicfields.serialnumber;
+
+import de.rub.nds.anvilcore.annotation.AnvilTest;
+import de.rub.nds.anvilcore.annotation.IpmLimitations;
+import de.rub.nds.anvilcore.annotation.TestStrength;
+import de.rub.nds.x509anvil.framework.annotation.ChainLength;
+import de.rub.nds.x509anvil.framework.annotation.RFC;
+import de.rub.nds.x509anvil.framework.annotation.SeverityLevel;
+import de.rub.nds.x509anvil.framework.anvil.X509AnvilTest;
+import de.rub.nds.x509anvil.framework.anvil.X509VerifierRunner;
+import de.rub.nds.x509anvil.framework.constants.Severity;
+import de.rub.nds.x509anvil.framework.verifier.VerifierException;
+import de.rub.nds.x509anvil.framework.verifier.VerifierResult;
+import de.rub.nds.x509anvil.framework.x509.config.X509CertificateChainConfig;
+import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
+import de.rub.nds.x509anvil.suite.tests.util.Modifiers;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+
+import java.math.BigInteger;
+
+public class ZeroSerialNumberTests extends X509AnvilTest {
+
+    @RFC(number = 5280, section = "4.1.2.1. Version",
+            text = "The serial number MUST be a positive integer assigned by the CA to each certificate. [...] Note: Non-conforming CAs may " +
+                    "issue certificates with serial numbers that are negative or zero.  Certificate users SHOULD be prepared to " +
+                    "gracefully handle such certificates.")
+    @SeverityLevel(Severity.INFORMATIONAL)
+    @ChainLength(minLength = 2, maxLength = 3, intermediateCertsModeled = 2)
+    @TestStrength(2)
+    @IpmLimitations(identifiers = "entity.serial_number")
+    @AnvilTest
+    public void zeroSerialNumberEntity(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        X509CertificateChainConfig certificateChainConfig = prepareConfig(argumentsAccessor, testRunner);
+        VerifierResult result = testRunner.execute(certificateChainConfig, Modifiers.illegalSerialNumberModifier(true, BigInteger.valueOf(0)));
+        Assertions.assertFalse(result.isValid());
+    }
+
+    @RFC(number = 5280, section = "4.1.2.1. Version",
+            text = "The serial number MUST be a positive integer assigned by the CA to each certificate. [...] Note: Non-conforming CAs may " +
+                    "issue certificates with serial numbers that are negative or zero.  Certificate users SHOULD be prepared to " +
+                    "gracefully handle such certificates.")
+    @SeverityLevel(Severity.INFORMATIONAL)
+    @ChainLength(minLength = 3, maxLength = 3, intermediateCertsModeled = 2)
+    @TestStrength(2)
+    @IpmLimitations(identifiers = "inter0.serial_number")
+    @AnvilTest
+    public void zeroSerialNumberIntermediate(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        X509CertificateChainConfig certificateChainConfig = prepareConfig(argumentsAccessor, testRunner);
+        VerifierResult result = testRunner.execute(certificateChainConfig, Modifiers.illegalSerialNumberModifier(false, BigInteger.valueOf(0)));
+        Assertions.assertFalse(result.isValid());
+    }
+}
--- X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/util/Modifiers.java
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/util/Modifiers.java
@@ -16,4 +16,14 @@ public class Modifiers {
             }
         };
     }
+
+    public static X509CertificateModifier illegalSerialNumberModifier(boolean entity, BigInteger version) {
+        return (certificate, config, previousConfig) -> {
+            if (entity && config.isEntity() || !entity && config.isIntermediate()) {
+                Asn1Integer serialNumberAsn1 = (Asn1Integer) X509Util.getAsn1ElementByIdentifierPath(certificate,
+                        "tbsCertificate", "serialNumber");
+                serialNumberAsn1.setValue(version);
+            }
+        };
+    }
 }
