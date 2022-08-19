659eb2c0416254f998a28e75672a671586940a48
Jonas <jonasthi97@gmail.com>
2022-08-19 17:33:29 +0200

    Add tbsSignature tests

--- X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/basicfields/TbsSignatureTests.java
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/basicfields/TbsSignatureTests.java
@@ -6,23 +6,20 @@ import de.rub.nds.asn1.model.Asn1ObjectIdentifier;
 import de.rub.nds.x509anvil.framework.annotation.ChainLength;
 import de.rub.nds.x509anvil.framework.annotation.RFC;
 import de.rub.nds.x509anvil.framework.annotation.SeverityLevel;
-import de.rub.nds.x509anvil.framework.anvil.ContextHelper;
 import de.rub.nds.x509anvil.framework.anvil.X509AnvilTest;
 import de.rub.nds.x509anvil.framework.anvil.X509VerifierRunner;
 import de.rub.nds.x509anvil.framework.constants.Severity;
-import de.rub.nds.x509anvil.framework.constants.SignatureAlgorithm;
-import de.rub.nds.x509anvil.framework.featureextraction.FeatureReport;
 import de.rub.nds.x509anvil.framework.verifier.VerifierException;
 import de.rub.nds.x509anvil.framework.verifier.VerifierResult;
 import de.rub.nds.x509anvil.framework.x509.config.X509CertificateChainConfig;
 import de.rub.nds.x509anvil.framework.x509.config.X509Util;
 import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
 import de.rub.nds.x509anvil.framework.x509.generator.X509CertificateModifier;
+import de.rub.nds.x509anvil.suite.tests.util.TestUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
-import org.junit.platform.commons.JUnitException;
 
-public class SignatureTests extends X509AnvilTest {
+public class TbsSignatureTests extends X509AnvilTest {
 
     @RFC(number = 5280, section = "4.1.2.3.  Signature",
             text = "This field MUST contain the same algorithm identifier as the signatureAlgorithm field in the sequence Certificate (Section 4.1.1.2).")
@@ -48,25 +45,45 @@ public class SignatureTests extends X509AnvilTest {
         Assertions.assertFalse(result.isValid());
     }
 
+    @RFC(number = 5280, section = "4.1.2.3.  Signature")
+    @SeverityLevel(Severity.ERROR)
+    @ChainLength(minLength = 2, maxLength = 3, intermediateCertsModeled = 2)
+    @TestStrength(2)
+    @AnvilTest(description = "Checks if the application rejects certificates where the signature algorithm in tbsCertificate.signature is unknown")
+    public void unknownOidEntity(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        X509CertificateChainConfig certificateChainConfig = prepareConfig(argumentsAccessor, testRunner);
+        VerifierResult result = testRunner.execute(certificateChainConfig, createUnknownOidModifier(true));
+        Assertions.assertFalse(result.isValid());
+    }
+
+    @RFC(number = 5280, section = "4.1.2.3.  Signature")
+    @SeverityLevel(Severity.ERROR)
+    @ChainLength(minLength = 3, maxLength = 3, intermediateCertsModeled = 2)
+    @TestStrength(2)
+    @AnvilTest(description = "Checks if the application rejects certificates where the signature algorithm in tbsCertificate.signature is unknown")
+    public void unknownOidIntermediate(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        X509CertificateChainConfig certificateChainConfig = prepareConfig(argumentsAccessor, testRunner);
+        VerifierResult result = testRunner.execute(certificateChainConfig, createUnknownOidModifier(false));
+        Assertions.assertFalse(result.isValid());
+    }
+
     public static X509CertificateModifier createTbsSignatureDoesntMatchSignatureAlgorithmModifier(boolean entity) {
         return (certificate, config, previousConfig) -> {
             if (entity && config.isEntity() || !entity && config.isIntermediate()) {
                 Asn1ObjectIdentifier tbsSignatureAsn1 = (Asn1ObjectIdentifier) X509Util.getAsn1ElementByIdentifierPath(certificate,
                         "tbsCertificate", "signature", "algorithm");
-                tbsSignatureAsn1.setValue(getNonMatchingAlgorithmOid(previousConfig.getSignatureAlgorithm()));
+                tbsSignatureAsn1.setValue(TestUtils.getNonMatchingAlgorithmOid(previousConfig.getSignatureAlgorithm()));
             }
         };
     }
 
-    /**
-     * Returns a supported signature algorithm oid that does not match the actually used algorithm
-     */
-    public static String getNonMatchingAlgorithmOid(SignatureAlgorithm actualAlgorithm) {
-        FeatureReport featureReport = ContextHelper.getContextDelegate().getFeatureReport();
-        SignatureAlgorithm nonMatchingSignatureAlgorithm = featureReport.getSupportedAlgorithms().stream()
-                .filter(a -> a != actualAlgorithm)
-                .findFirst()
-                .orElseThrow(() -> new JUnitException("No other algorithm supported"));
-        return nonMatchingSignatureAlgorithm.getOid();
+    public static X509CertificateModifier createUnknownOidModifier(boolean entity) {
+        return (certificate, config, previousConfig) -> {
+            if (entity && config.isEntity() || !entity && config.isIntermediate()) {
+                Asn1ObjectIdentifier tbsSignatureAsn1 = (Asn1ObjectIdentifier) X509Util.getAsn1ElementByIdentifierPath(certificate,
+                        "tbsCertificate", "signature", "algorithm");
+                tbsSignatureAsn1.setValue("1.2.3.4.5.6.7.8");
+            }
+        };
     }
 }
