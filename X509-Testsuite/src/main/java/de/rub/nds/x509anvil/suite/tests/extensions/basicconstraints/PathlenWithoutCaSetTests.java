e2419539976bc123468c3c9167c4447c1e73e817
Jonas <jonasthi97@gmail.com>
2022-08-22 16:34:30 +0200

    Add tests for when pathlen is present even if CA=false

---
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/extensions/basicconstraints/PathlenWithoutCaSetTests.java
@@ -0,0 +1,57 @@
+package de.rub.nds.x509anvil.suite.tests.extensions.basicconstraints;
+
+import de.rub.nds.anvilcore.annotation.AnvilTest;
+import de.rub.nds.anvilcore.annotation.TestStrength;
+import de.rub.nds.anvilcore.annotation.ValueConstraint;
+import de.rub.nds.x509anvil.framework.annotation.ChainLength;
+import de.rub.nds.x509anvil.framework.annotation.RFC;
+import de.rub.nds.x509anvil.framework.annotation.SeverityLevel;
+import de.rub.nds.x509anvil.framework.anvil.X509AnvilTest;
+import de.rub.nds.x509anvil.framework.anvil.X509VerifierRunner;
+import de.rub.nds.x509anvil.framework.constants.ExtensionType;
+import de.rub.nds.x509anvil.framework.constants.Severity;
+import de.rub.nds.x509anvil.framework.verifier.VerifierException;
+import de.rub.nds.x509anvil.framework.verifier.VerifierResult;
+import de.rub.nds.x509anvil.framework.x509.config.X509CertificateChainConfig;
+import de.rub.nds.x509anvil.framework.x509.config.extension.BasicConstraintsExtensionConfig;
+import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
+import de.rub.nds.x509anvil.suite.tests.util.Constraints;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+
+public class PathlenWithoutCaSetTests extends X509AnvilTest {
+
+    @RFC(number = 5280, section = "4.2.1.9. Basic Constraints",
+            text = "CAs MUST NOT include the pathLenConstraint field unless the cA boolean is asserted")
+    @SeverityLevel(Severity.INFORMATIONAL)
+    @ChainLength(minLength = 2, maxLength = 3, intermediateCertsModeled = 2)
+    @TestStrength(2)
+    @ValueConstraint(identifier = "entity.ext_basic_constraints_ca", clazz = Constraints.class, method = "strictlyDisabled")
+    @AnvilTest
+    public void pathlenWithoutCaSetEntity(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        X509CertificateChainConfig chainConfig = prepareConfig(argumentsAccessor, testRunner);
+        BasicConstraintsExtensionConfig basicConstraintsExtensionConfig = (BasicConstraintsExtensionConfig)
+                chainConfig.getEntityCertificateConfig().extension(ExtensionType.BASIC_CONSTRAINTS);
+        basicConstraintsExtensionConfig.setPathLenConstraintPresent(true);
+        basicConstraintsExtensionConfig.setPathLenConstraint(1);
+        VerifierResult result = testRunner.execute(chainConfig);
+        Assertions.assertFalse(result.isValid());
+    }
+
+    @RFC(number = 5280, section = "4.2.1.9. Basic Constraints",
+            text = "CAs MUST NOT include the pathLenConstraint field unless the cA boolean is asserted")
+    @SeverityLevel(Severity.INFORMATIONAL)
+    @ChainLength(minLength = 3, maxLength = 3, intermediateCertsModeled = 2)
+    @TestStrength(2)
+    @ValueConstraint(identifier = "inter0.ext_basic_constraints_ca", clazz = Constraints.class, method = "strictlyDisabled")
+    @AnvilTest
+    public void pathlenWithoutCaSetIntermediate(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        X509CertificateChainConfig chainConfig = prepareConfig(argumentsAccessor, testRunner);
+        BasicConstraintsExtensionConfig basicConstraintsExtensionConfig = (BasicConstraintsExtensionConfig)
+                chainConfig.getIntermediateConfig(0).extension(ExtensionType.BASIC_CONSTRAINTS);
+        basicConstraintsExtensionConfig.setPathLenConstraintPresent(true);
+        basicConstraintsExtensionConfig.setPathLenConstraint(1);
+        VerifierResult result = testRunner.execute(chainConfig);
+        Assertions.assertFalse(result.isValid());
+    }
+}
--- X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/util/Constraints.java
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/util/Constraints.java
@@ -3,14 +3,22 @@ package de.rub.nds.x509anvil.suite.tests.util;
 public class Constraints {
 
     public static boolean enabled(Boolean value) {
+        if (value == null) return false;
         return value;
     }
 
     public static boolean disabled(Boolean value) {
+        if (value == null) return true;
+        return !value;
+    }
+
+    public static boolean strictlyDisabled(Boolean value) {
+        if (value == null) return false;
         return !value;
     }
 
     public static boolean allowVersion1(Integer value) {
+        if (value == null) return false;
         return value.equals(0);
     }
 }
