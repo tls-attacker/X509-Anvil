dd03f1c3086f7dbcaa8802a64bccb5d9272ea8cc
Jonas <jonasthi97@gmail.com>
2022-08-22 16:56:30 +0200

    Add tests for critical authority key id extension

---
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/extensions/authoritykeyid/CriticalAuthorityKeyIdTests.java
@@ -0,0 +1,48 @@
+package de.rub.nds.x509anvil.suite.tests.extensions.authoritykeyid;
+
+import de.rub.nds.anvilcore.annotation.AnvilTest;
+import de.rub.nds.anvilcore.annotation.TestStrength;
+import de.rub.nds.anvilcore.annotation.ValueConstraint;
+import de.rub.nds.x509anvil.framework.annotation.ChainLength;
+import de.rub.nds.x509anvil.framework.annotation.RFC;
+import de.rub.nds.x509anvil.framework.annotation.SeverityLevel;
+import de.rub.nds.x509anvil.framework.anvil.X509AnvilTest;
+import de.rub.nds.x509anvil.framework.anvil.X509VerifierRunner;
+import de.rub.nds.x509anvil.framework.constants.ExtensionType;
+import de.rub.nds.x509anvil.framework.constants.Severity;
+import de.rub.nds.x509anvil.framework.verifier.VerifierException;
+import de.rub.nds.x509anvil.framework.verifier.VerifierResult;
+import de.rub.nds.x509anvil.framework.x509.config.X509CertificateChainConfig;
+import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
+import de.rub.nds.x509anvil.suite.tests.util.Constraints;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+
+public class CriticalAuthorityKeyIdTests extends X509AnvilTest {
+
+    @RFC(number = 5280, section = "4.2.1.1. Authority Key Identifier", text = "Conforming CAs MUST mark this extension as non-critical.")
+    @SeverityLevel(Severity.INFORMATIONAL)
+    @ChainLength(minLength = 2, maxLength = 3)
+    @TestStrength(2)
+    @AnvilTest
+    @ValueConstraint(identifier = "entity.ext_authority_key_identifier_present", clazz = Constraints.class, method = "enabled")
+    public void criticalAuthorityKeyIdEntity(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        X509CertificateChainConfig chainConfig = prepareConfig(argumentsAccessor, testRunner);
+        chainConfig.getEntityCertificateConfig().extension(ExtensionType.AUTHORITY_KEY_IDENTIFIER).setCritical(true);
+        VerifierResult result = testRunner.execute(chainConfig);
+        Assertions.assertFalse(result.isValid());
+    }
+
+    @RFC(number = 5280, section = "4.2.1.1. Authority Key Identifier", text = "Conforming CAs MUST mark this extension as non-critical.")
+    @SeverityLevel(Severity.INFORMATIONAL)
+    @ChainLength(minLength = 3, maxLength = 3)
+    @TestStrength(2)
+    @AnvilTest
+    @ValueConstraint(identifier = "inter0.ext_authority_key_identifier_present", clazz = Constraints.class, method = "enabled")
+    public void criticalAuthorityKeyIdIntermediate(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        X509CertificateChainConfig chainConfig = prepareConfig(argumentsAccessor, testRunner);
+        chainConfig.getIntermediateConfig(0).extension(ExtensionType.AUTHORITY_KEY_IDENTIFIER).setCritical(true);
+        VerifierResult result = testRunner.execute(chainConfig);
+        Assertions.assertFalse(result.isValid());
+    }
+}
