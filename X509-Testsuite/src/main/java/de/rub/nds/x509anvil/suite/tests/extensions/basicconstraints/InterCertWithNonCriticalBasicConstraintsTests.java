f78a16ca0c33d7704ae1e5591c4de5bc2567d910
Jonas <jonasthi97@gmail.com>
2022-08-22 16:47:19 +0200

    Add test for intermediate certs with non-critical basic constraints extension

---
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/extensions/basicconstraints/InterCertWithNonCriticalBasicConstraintsTests.java
@@ -0,0 +1,33 @@
+package de.rub.nds.x509anvil.suite.tests.extensions.basicconstraints;
+
+import de.rub.nds.anvilcore.annotation.AnvilTest;
+import de.rub.nds.anvilcore.annotation.TestStrength;
+import de.rub.nds.x509anvil.framework.annotation.ChainLength;
+import de.rub.nds.x509anvil.framework.annotation.RFC;
+import de.rub.nds.x509anvil.framework.annotation.SeverityLevel;
+import de.rub.nds.x509anvil.framework.anvil.X509AnvilTest;
+import de.rub.nds.x509anvil.framework.anvil.X509VerifierRunner;
+import de.rub.nds.x509anvil.framework.constants.ExtensionType;
+import de.rub.nds.x509anvil.framework.constants.Severity;
+import de.rub.nds.x509anvil.framework.verifier.VerifierException;
+import de.rub.nds.x509anvil.framework.verifier.VerifierResult;
+import de.rub.nds.x509anvil.framework.x509.config.X509CertificateChainConfig;
+import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+
+public class InterCertWithNonCriticalBasicConstraintsTests extends X509AnvilTest {
+
+    @RFC(number = 5280, section = "4.2.1.9. Basic Constraints",
+            text = "in all CA certificates that contain public keys used to validate digital signatures on certificates [...] MUST mark the extension as critical in such certificates.")
+    @SeverityLevel(Severity.WARNING)
+    @ChainLength(minLength = 3, maxLength = 3, intermediateCertsModeled = 2)
+    @TestStrength(2)
+    @AnvilTest
+    public void intermediateCertWithNonCriticalBasicConstraints(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        X509CertificateChainConfig chainConfig = prepareConfig(argumentsAccessor, testRunner);
+        chainConfig.getIntermediateConfig(0).extension(ExtensionType.BASIC_CONSTRAINTS).setCritical(false);
+        VerifierResult result = testRunner.execute(chainConfig);
+        Assertions.assertFalse(result.isValid());
+    }
+}
