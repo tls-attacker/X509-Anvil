edbcf829d22e9c617f54fdb1433938fd2a0a1604
Jonas <jonasthi97@gmail.com>
2022-08-25 11:19:46 +0200

    Add tests for self-signed non-root certificates

---
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/chain/SelfSignedNonRootTests.java
@@ -0,0 +1,42 @@
+package de.rub.nds.x509anvil.suite.tests.chain;
+
+import de.rub.nds.anvilcore.annotation.AnvilTest;
+import de.rub.nds.x509anvil.framework.annotation.ChainLength;
+import de.rub.nds.x509anvil.framework.annotation.SeverityLevel;
+import de.rub.nds.x509anvil.framework.annotation.Specification;
+import de.rub.nds.x509anvil.framework.anvil.X509AnvilTest;
+import de.rub.nds.x509anvil.framework.anvil.X509VerifierRunner;
+import de.rub.nds.x509anvil.framework.constants.Severity;
+import de.rub.nds.x509anvil.framework.verifier.VerifierException;
+import de.rub.nds.x509anvil.framework.verifier.VerifierResult;
+import de.rub.nds.x509anvil.framework.x509.config.X509CertificateChainConfig;
+import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+
+public class SelfSignedNonRootTests extends X509AnvilTest {
+
+    @Specification(document = "RFC 5280", section = "6.1 Basic Path Validation",
+            text = "for all x in {1, ..., n-1}, the subject of certificate x is the issuer of certificate x+1;")
+    @SeverityLevel(Severity.CRITICAL)
+    @ChainLength(minLength = 2, maxLength = 3, intermediateCertsModeled = 2)
+    @AnvilTest
+    public void selfSignedEntity(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        X509CertificateChainConfig certificateChainConfig = prepareConfig(argumentsAccessor, testRunner);
+        certificateChainConfig.getEntityCertificateConfig().setSelfSigned(true);
+        VerifierResult result = testRunner.execute(certificateChainConfig);
+        Assertions.assertFalse(result.isValid());
+    }
+
+    @Specification(document = "RFC 5280", section = "6.1 Basic Path Validation",
+            text = "for all x in {1, ..., n-1}, the subject of certificate x is the issuer of certificate x+1;")
+    @SeverityLevel(Severity.CRITICAL)
+    @ChainLength(minLength = 3, maxLength = 3, intermediateCertsModeled = 2)
+    @AnvilTest
+    public void selfSignedIntermediate(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        X509CertificateChainConfig certificateChainConfig = prepareConfig(argumentsAccessor, testRunner);
+        certificateChainConfig.getIntermediateConfig(0).setSelfSigned(true);
+        VerifierResult result = testRunner.execute(certificateChainConfig);
+        Assertions.assertFalse(result.isValid());
+    }
+}
