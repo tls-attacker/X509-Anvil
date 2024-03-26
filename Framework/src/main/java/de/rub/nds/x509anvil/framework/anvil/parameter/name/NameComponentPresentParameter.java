5fdef12166d507ce18b5f321de32281982809b7d
Felix Lange <felix.lange@uni-paderborn.de>
2024-03-26 11:26:09 +0100

    refactor: name component parameter

--- Framework/src/main/java/de/rub/nds/x509anvil/framework/anvil/parameter/name/NameComponentPresentParameter.java
+++ Framework/src/main/java/de/rub/nds/x509anvil/framework/anvil/parameter/name/NameComponentPresentParameter.java
@@ -12,10 +12,18 @@ package de.rub.nds.x509anvil.framework.anvil.parameter.name;
 import de.rub.nds.anvilcore.model.DerivationScope;
 import de.rub.nds.anvilcore.model.parameter.DerivationParameter;
 import de.rub.nds.anvilcore.model.parameter.ParameterIdentifier;
+import de.rub.nds.protocol.xml.Pair;
 import de.rub.nds.x509anvil.framework.anvil.parameter.BooleanCertificateSpecificParameter;
 import de.rub.nds.x509anvil.framework.x509.config.X509CertificateChainConfig;
 import de.rub.nds.x509anvil.framework.x509.config.X509CertificateConfig;
 import de.rub.nds.x509anvil.framework.x509.config.model.DirectoryStringType;
+import de.rub.nds.x509attacker.constants.X500AttributeType;
+import de.rub.nds.x509attacker.x509.model.RelativeDistinguishedName;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class NameComponentPresentParameter extends BooleanCertificateSpecificParameter {
     private final String nameComponentOid;
@@ -38,7 +46,12 @@ public class NameComponentPresentParameter extends BooleanCertificateSpecificPar
     @Override
     protected void applyToCertificateConfig(X509CertificateConfig certificateConfig, DerivationScope derivationScope) {
         if (getSelectedValue()) {
-            certificateConfig.getSubject().addNameComponent(nameComponentOid, value, DirectoryStringType.PRINTABLE);
+            List<Pair<X500AttributeType, String>> name = new LinkedList<>();
+            name.add(new Pair<>(Arrays.stream(X500AttributeType.values())
+                    .filter(x500AttributeType -> x500AttributeType.getOid().toString().equals(nameComponentOid))
+                    .collect(Collectors.toList()).get(0), value));
+            RelativeDistinguishedName nameToAdd = new RelativeDistinguishedName("relativeDistinguishedName", name);
+            certificateConfig.getSubject().addRelativeDistinguishedNames(nameToAdd);
         }
     }
 
