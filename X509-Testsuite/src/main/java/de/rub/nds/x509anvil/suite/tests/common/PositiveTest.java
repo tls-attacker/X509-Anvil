dfdeb41062347ba057e44fa2f6d4b59e7eb38262
JonSnowWhite <nniklas@mail.upb.de>
2024-09-17 13:22:13 +0200

    fixed sampleTestCase

--- X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/common/PositiveTest.java
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/common/PositiveTest.java
@@ -2,31 +2,27 @@ package de.rub.nds.x509anvil.suite.tests.common;
 
 import de.rub.nds.anvilcore.annotation.AnvilTest;
 import de.rub.nds.anvilcore.annotation.TestStrength;
-import de.rub.nds.tlsattacker.core.config.Config;
 import de.rub.nds.x509anvil.framework.annotation.ChainLength;
+import de.rub.nds.x509anvil.framework.annotation.SeverityLevel;
+import de.rub.nds.x509anvil.framework.annotation.Specification;
 import de.rub.nds.x509anvil.framework.anvil.X509AnvilTest;
 import de.rub.nds.x509anvil.framework.anvil.X509VerifierRunner;
+import de.rub.nds.x509anvil.framework.constants.Severity;
 import de.rub.nds.x509anvil.framework.verifier.VerifierException;
 import de.rub.nds.x509anvil.framework.verifier.VerifierResult;
 import de.rub.nds.x509anvil.framework.x509.config.X509CertificateChainConfig;
 import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 
 public class PositiveTest extends X509AnvilTest {
-    @AnvilTest()
+    @AnvilTest(id = "1")
     @ChainLength(minLength = 2, maxLength = 4, intermediateCertsModeled = 2)
-    @TestStrength(2)
-    public void sampleTestCase(ArgumentsAccessor argumentsAccessor, X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
-        X509CertificateChainConfig config = prepareConfig(argumentsAccessor, testRunner);
+    @TestStrength(1)
+    @Specification(document = "None", section = "None", text = "This is a sample test case to test functionality.")
+    @SeverityLevel(Severity.WARNING)
+    public void sampleTestCase(X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        X509CertificateChainConfig config = prepareConfig(testRunner);
         VerifierResult result = testRunner.execute(config);
         Assertions.assertTrue(result.isValid());
-    }
-
-    @Test
-    public void simpleTest() {
-        Config config = new Config();
-        config.createCopy();
     }
 }
