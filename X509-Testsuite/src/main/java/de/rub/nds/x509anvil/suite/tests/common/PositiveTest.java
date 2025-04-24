81671a2c3f4fddc864b6c8f3d4e0ee7a08801070
JonSnowWhite <nniklas@mail.upb.de>
2025-04-24 13:24:23 +0200

    added certificate generation test

--- X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/common/PositiveTest.java
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/common/PositiveTest.java
@@ -6,8 +6,20 @@ import de.rub.nds.x509anvil.framework.annotation.ChainLength;
 import de.rub.nds.x509anvil.framework.anvil.X509AnvilTest;
 import de.rub.nds.x509anvil.framework.anvil.X509VerifierRunner;
 import de.rub.nds.x509anvil.framework.verifier.VerifierException;
+import de.rub.nds.x509anvil.framework.x509.config.X509CertificateChainConfig;
+import de.rub.nds.x509anvil.framework.x509.config.X509CertificateConfigUtil;
+import de.rub.nds.x509anvil.framework.x509.config.X509Util;
 import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
+import de.rub.nds.x509anvil.framework.x509.generator.X509CertificateChainGenerator;
 import de.rub.nds.x509anvil.framework.x509.generator.modifier.X509CertificateConfigModifier;
+import de.rub.nds.x509attacker.chooser.X509Chooser;
+import de.rub.nds.x509attacker.config.X509CertificateConfig;
+import de.rub.nds.x509attacker.constants.CertificateChainPositionType;
+import de.rub.nds.x509attacker.context.X509Context;
+import de.rub.nds.x509attacker.x509.model.X509Certificate;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 public class PositiveTest extends X509AnvilTest {
 
