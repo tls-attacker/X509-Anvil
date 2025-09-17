ad03e5be9aa665a5bfbd8936fb10585a4a6adc15
JonSnowWhite <nniklas@mail.upb.de>
2025-09-17 14:59:36 +0200

    policy mapping

--- X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/extensions/certificatepolicies/DuplicatePoliciesTests.java
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/extensions/certificatepolicies/DuplicatePoliciesTests.java
@@ -2,6 +2,7 @@ package de.rub.nds.x509anvil.suite.tests.extensions.certificatepolicies;
 
 import de.rub.nds.anvilcore.annotation.AnvilTest;
 import de.rub.nds.anvilcore.annotation.IpmLimitations;
+import de.rub.nds.asn1.oid.ObjectIdentifier;
 import de.rub.nds.x509anvil.framework.annotation.ChainLength;
 import de.rub.nds.x509anvil.framework.anvil.X509AnvilTest;
 import de.rub.nds.x509anvil.framework.anvil.X509VerifierRunner;
@@ -9,14 +10,16 @@ import de.rub.nds.x509anvil.framework.verifier.VerifierException;
 import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
 import de.rub.nds.x509anvil.framework.x509.generator.modifier.X509CertificateConfigModifier;
 import de.rub.nds.x509attacker.config.extension.CertificatePoliciesConfig;
+import de.rub.nds.x509attacker.x509.model.extensions.PolicyQualifierInfo;
 import de.rub.nds.x509attacker.x509.model.extensions.PolicyQualifiers;
 
 import java.util.List;
 
 public class DuplicatePoliciesTests extends X509AnvilTest {
+
     @ChainLength(minLength = 2)
     @IpmLimitations(identifiers = "entity:extensions_present")
-    @AnvilTest(id = "extension-cb233ef8a5")
+    @AnvilTest(id = "extension-6b125b4c97")
     public void duplicatePoliciesEntity(X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
         assertInvalid(testRunner, true, (X509CertificateConfigModifier) config -> {
             CertificatePoliciesConfig certificatePoliciesConfig = new CertificatePoliciesConfig();
@@ -31,7 +34,7 @@ public class DuplicatePoliciesTests extends X509AnvilTest {
     }
 
     @ChainLength(minLength = 3)
-    @AnvilTest(id = "extension-bc135fe2a1")
+    @AnvilTest(id = "extension-c3e50f1c30")
     public void duplicatePoliciesIntermediate(X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
         assertInvalid(testRunner, false, (X509CertificateConfigModifier) config -> {
             CertificatePoliciesConfig certificatePoliciesConfig = new CertificatePoliciesConfig();
@@ -43,4 +46,54 @@ public class DuplicatePoliciesTests extends X509AnvilTest {
             config.addExtensions(certificatePoliciesConfig);
         });
     }
+
+    @ChainLength(minLength = 2)
+    @IpmLimitations(identifiers = "entity:extensions_present")
+    @AnvilTest(id = "extension-bf8b1694b7")
+    public void undefinedAnyPolicyEntity(X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        assertInvalid(testRunner, true, (X509CertificateConfigModifier) config -> {
+            CertificatePoliciesConfig certificatePoliciesConfig = new CertificatePoliciesConfig();
+            certificatePoliciesConfig.setPresent(true);
+            certificatePoliciesConfig.setCritical(true);
+
+            certificatePoliciesConfig.setPolicyIdentifiers(List.of("2.5.29.32.0"));
+
+            PolicyQualifiers qualifiers = new PolicyQualifiers("qualifiers");
+            PolicyQualifierInfo policyQualifierInfo = new PolicyQualifierInfo("info");
+            policyQualifierInfo.setPolicyObjectIdentifier(new ObjectIdentifier(new byte[] { 1, 3, 6, 1, 5, 5, 7, 2, 1 }));
+            policyQualifierInfo.setQualifierOctetString(new byte[] {1, 2, 3, 4});
+            qualifiers.setPolicyQualifierInfo(List.of(policyQualifierInfo));
+
+
+            certificatePoliciesConfig.setPolicyQualifiers(List.of(qualifiers));
+            certificatePoliciesConfig.setIncludeQualifiers(List.of(true));
+            config.addExtensions(certificatePoliciesConfig);
+            config.setIncludeExtensions(true);
+        });
+    }
+
+    @ChainLength(minLength = 3)
+    @AnvilTest(id = "extension-bada689d12")
+    public void undefinedAnyPolicyIntermediate(X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        assertInvalid(testRunner, false, (X509CertificateConfigModifier) config -> {
+            CertificatePoliciesConfig certificatePoliciesConfig = new CertificatePoliciesConfig();
+            certificatePoliciesConfig.setPresent(true);
+            certificatePoliciesConfig.setCritical(true);
+            certificatePoliciesConfig.setPolicyIdentifiers(List.of("2.5.29.32.0"));
+
+            PolicyQualifiers qualifiers = new PolicyQualifiers("qualifiers");
+            PolicyQualifierInfo policyQualifierInfo = new PolicyQualifierInfo("info");
+            policyQualifierInfo.setPolicyObjectIdentifier(new ObjectIdentifier(new byte[] { 1, 3, 6, 1, 5, 5, 7, 2, 1 }));
+            policyQualifierInfo.setQualifierOctetString(new byte[] {1, 2, 3, 4});
+            qualifiers.setPolicyQualifierInfo(List.of(policyQualifierInfo));
+
+
+            certificatePoliciesConfig.setPolicyQualifiers(List.of(qualifiers));
+            certificatePoliciesConfig.setIncludeQualifiers(List.of(true));
+            config.addExtensions(certificatePoliciesConfig);
+            config.setIncludeExtensions(true);
+        });
+    }
+
+    // TODO: usernotice MUST statement
 }
--- X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/extensions/policymappings/NonCriticalPolicyMappingsTests.java
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/extensions/policymappings/NonCriticalPolicyMappingsTests.java
@@ -1,27 +1,172 @@
 package de.rub.nds.x509anvil.suite.tests.extensions.policymappings;
 
 import de.rub.nds.anvilcore.annotation.AnvilTest;
+import de.rub.nds.anvilcore.annotation.IpmLimitations;
+import de.rub.nds.asn1.oid.ObjectIdentifier;
 import de.rub.nds.x509anvil.framework.annotation.ChainLength;
 import de.rub.nds.x509anvil.framework.anvil.X509AnvilTest;
 import de.rub.nds.x509anvil.framework.anvil.X509VerifierRunner;
 import de.rub.nds.x509anvil.framework.verifier.VerifierException;
 import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
 import de.rub.nds.x509anvil.framework.x509.generator.modifier.X509CertificateConfigModifier;
+import de.rub.nds.x509attacker.config.extension.CertificatePoliciesConfig;
 import de.rub.nds.x509attacker.config.extension.PolicyMappingsConfig;
+import de.rub.nds.x509attacker.x509.model.extensions.PolicyQualifierInfo;
+import de.rub.nds.x509attacker.x509.model.extensions.PolicyQualifiers;
 
 import java.util.List;
 
 public class NonCriticalPolicyMappingsTests extends X509AnvilTest {
-    @ChainLength(minLength = 3)
-    @AnvilTest(id = "extension-12c3ea29d2")
-    public void nonCriticalPolicyMappingsIntermediate(X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
-        assertInvalid(testRunner, false, (X509CertificateConfigModifier) config -> {
+
+    // When qualifiers are used with the special policy anyPolicy, they MUST be limited to the qualifiers identified in this section
+    @ChainLength(minLength = 2)
+    @IpmLimitations(identifiers = "entity:extensions_present")
+    @AnvilTest(id = "extension-7b8f543a1f")
+    public void anyPolicyMappingFromEntityToIssuerInEntity(X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        assertInvalid(testRunner, true, (X509CertificateConfigModifier) config -> {
+            CertificatePoliciesConfig certificatePoliciesConfig = new CertificatePoliciesConfig();
+            certificatePoliciesConfig.setPresent(true);
+            certificatePoliciesConfig.setCritical(true);
+
+            certificatePoliciesConfig.setPolicyIdentifiers(List.of("2.5.29.32.0"));
+
+            certificatePoliciesConfig.setPolicyQualifiers(List.of(new PolicyQualifiers("qualifiers")));
+            certificatePoliciesConfig.setIncludeQualifiers(List.of(false));
+            config.addExtensions(certificatePoliciesConfig);
+            config.setIncludeExtensions(true);
+
             PolicyMappingsConfig policyMappingsConfig = new PolicyMappingsConfig();
             policyMappingsConfig.setPresent(true);
             policyMappingsConfig.setCritical(false);
-            policyMappingsConfig.setIssuerDomainPolicies(List.of("2.23.140.1.2.1", "2.23.140.1.2.2"));
-            policyMappingsConfig.setSubjectDomainPolicies(List.of("2.23.140.1.2.1", "2.23.140.1.2.2"));
+            policyMappingsConfig.setSubjectDomainPolicies(List.of("2.5.29.32.0"));
+            policyMappingsConfig.setIssuerDomainPolicies(List.of("2.23.140.1.2.1"));
             config.addExtensions(policyMappingsConfig);
+
+
+        }, (X509CertificateConfigModifier) config -> {
+            CertificatePoliciesConfig certificatePoliciesConfig = new CertificatePoliciesConfig();
+            certificatePoliciesConfig.setPresent(true);
+            certificatePoliciesConfig.setCritical(true);
+
+            certificatePoliciesConfig.setPolicyIdentifiers(List.of("2.23.140.1.2.1"));
+
+            certificatePoliciesConfig.setPolicyQualifiers(List.of(new PolicyQualifiers("qualifiers")));
+            certificatePoliciesConfig.setIncludeQualifiers(List.of(false));
+            config.addExtensions(certificatePoliciesConfig);
+            config.setIncludeExtensions(true);
+        });
+    }
+
+    @ChainLength(minLength = 3)
+    @AnvilTest(id = "extension-42c0e99d7d")
+    public void anyPolicyMappingFromEntityToIssuerInIntermediate(X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        assertInvalid(testRunner, false, (X509CertificateConfigModifier) config -> {
+            CertificatePoliciesConfig certificatePoliciesConfig = new CertificatePoliciesConfig();
+            certificatePoliciesConfig.setPresent(true);
+            certificatePoliciesConfig.setCritical(true);
+
+            certificatePoliciesConfig.setPolicyIdentifiers(List.of("2.5.29.32.0"));
+
+            certificatePoliciesConfig.setPolicyQualifiers(List.of(new PolicyQualifiers("qualifiers")));
+            certificatePoliciesConfig.setIncludeQualifiers(List.of(false));
+            config.addExtensions(certificatePoliciesConfig);
+            config.setIncludeExtensions(true);
+
+            PolicyMappingsConfig policyMappingsConfig = new PolicyMappingsConfig();
+            policyMappingsConfig.setPresent(true);
+            policyMappingsConfig.setCritical(false);
+            policyMappingsConfig.setSubjectDomainPolicies(List.of("2.5.29.32.0"));
+            policyMappingsConfig.setIssuerDomainPolicies(List.of("2.23.140.1.2.1"));
+            config.addExtensions(policyMappingsConfig);
+
+
+        }, (X509CertificateConfigModifier) config -> {
+            CertificatePoliciesConfig certificatePoliciesConfig = new CertificatePoliciesConfig();
+            certificatePoliciesConfig.setPresent(true);
+            certificatePoliciesConfig.setCritical(true);
+
+            certificatePoliciesConfig.setPolicyIdentifiers(List.of("2.23.140.1.2.1"));
+
+            certificatePoliciesConfig.setPolicyQualifiers(List.of(new PolicyQualifiers("qualifiers")));
+            certificatePoliciesConfig.setIncludeQualifiers(List.of(false));
+            config.addExtensions(certificatePoliciesConfig);
+            config.setIncludeExtensions(true);
+        });
+    }
+
+    // When qualifiers are used with the special policy anyPolicy, they MUST be limited to the qualifiers identified in this section
+    @ChainLength(minLength = 2)
+    @IpmLimitations(identifiers = "entity:extensions_present")
+    @AnvilTest(id = "extension-587d94f3b5")
+    public void anyPolicyMappingFromIssuerToEntityInEntity(X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        assertInvalid(testRunner, true, (X509CertificateConfigModifier) config -> {
+            CertificatePoliciesConfig certificatePoliciesConfig = new CertificatePoliciesConfig();
+            certificatePoliciesConfig.setPresent(true);
+            certificatePoliciesConfig.setCritical(true);
+
+            certificatePoliciesConfig.setPolicyQualifiers(List.of(new PolicyQualifiers("qualifiers")));
+            certificatePoliciesConfig.setIncludeQualifiers(List.of(false));
+            config.addExtensions(certificatePoliciesConfig);
+            config.setIncludeExtensions(true);
+
+            certificatePoliciesConfig.setPolicyIdentifiers(List.of("2.23.140.1.2.1"));
+
+            PolicyMappingsConfig policyMappingsConfig = new PolicyMappingsConfig();
+            policyMappingsConfig.setPresent(true);
+            policyMappingsConfig.setCritical(false);
+            policyMappingsConfig.setIssuerDomainPolicies(List.of("2.5.29.32.0"));
+            policyMappingsConfig.setSubjectDomainPolicies(List.of("2.23.140.1.2.1"));
+            config.addExtensions(policyMappingsConfig);
+
+
+        }, (X509CertificateConfigModifier) config -> {
+            CertificatePoliciesConfig certificatePoliciesConfig = new CertificatePoliciesConfig();
+            certificatePoliciesConfig.setPresent(true);
+            certificatePoliciesConfig.setCritical(true);
+
+            certificatePoliciesConfig.setPolicyIdentifiers(List.of("2.5.29.32.0"));
+
+            certificatePoliciesConfig.setPolicyQualifiers(List.of(new PolicyQualifiers("qualifiers")));
+            certificatePoliciesConfig.setIncludeQualifiers(List.of(false));
+            config.addExtensions(certificatePoliciesConfig);
+            config.setIncludeExtensions(true);
+        });
+    }
+
+    @ChainLength(minLength = 3)
+    @AnvilTest(id = "extension-ab7837006f")
+    public void anyPolicyMappingFromIssuerToEntityInIntermediate(X509VerifierRunner testRunner) throws VerifierException, CertificateGeneratorException {
+        assertInvalid(testRunner, false, (X509CertificateConfigModifier) config -> {
+            CertificatePoliciesConfig certificatePoliciesConfig = new CertificatePoliciesConfig();
+            certificatePoliciesConfig.setPresent(true);
+            certificatePoliciesConfig.setCritical(true);
+
+            certificatePoliciesConfig.setPolicyQualifiers(List.of(new PolicyQualifiers("qualifiers")));
+            certificatePoliciesConfig.setIncludeQualifiers(List.of(false));
+            config.addExtensions(certificatePoliciesConfig);
+            config.setIncludeExtensions(true);
+
+            certificatePoliciesConfig.setPolicyIdentifiers(List.of("2.23.140.1.2.1"));
+
+            PolicyMappingsConfig policyMappingsConfig = new PolicyMappingsConfig();
+            policyMappingsConfig.setPresent(true);
+            policyMappingsConfig.setCritical(false);
+            policyMappingsConfig.setIssuerDomainPolicies(List.of("2.5.29.32.0"));
+            policyMappingsConfig.setSubjectDomainPolicies(List.of("2.23.140.1.2.1"));
+            config.addExtensions(policyMappingsConfig);
+
+
+        }, (X509CertificateConfigModifier) config -> {
+            CertificatePoliciesConfig certificatePoliciesConfig = new CertificatePoliciesConfig();
+            certificatePoliciesConfig.setPresent(true);
+            certificatePoliciesConfig.setCritical(true);
+
+            certificatePoliciesConfig.setPolicyIdentifiers(List.of("2.5.29.32.0"));
+
+            certificatePoliciesConfig.setPolicyQualifiers(List.of(new PolicyQualifiers("qualifiers")));
+            certificatePoliciesConfig.setIncludeQualifiers(List.of(false));
+            config.addExtensions(certificatePoliciesConfig);
+            config.setIncludeExtensions(true);
         });
     }
 }
