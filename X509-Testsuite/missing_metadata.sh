67cc3fd02cc121d443c0fa2a58fef3ebb00fcb2a
Felix Lange <felix.lange@uni-paderborn.de>
2025-10-09 16:32:47 +0200

    refactor: English

--- X509-Testsuite/missing_metadata.sh
+++ X509-Testsuite/missing_metadata.sh
@@ -1,16 +1,16 @@
-# 1. IDs aus den Java-Dateien holen
+# 1. Get IDs from all tests
 grep -rhoP '@AnvilTest\([^)]*id\s*=\s*"\K[^"]+' src/ \
   | grep -E '^(basic|extension)-[0-9a-f]{10}$' \
   | sort -u > ids.txt
 
 
-# 2. Keys aus metadata.json holen
+# 2. Get keys from json
 jq -r 'keys[]' ./src/main/resources/metadata.json | sort -u > meta.txt
 
-# 3. Differenz berechnen (IDs ohne Metadaten)
+# 3. Calculate differences
 comm -23 ids.txt meta.txt > ids_without_meta.txt
 
-# 4. Ausgabe + Anzahl
+# 4. Print and count
 cat ids_without_meta.txt
 count=$(wc -l < ids_without_meta.txt)
 
