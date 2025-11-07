238938a2bf14f2d546708e860c94bb39afcad9cd
Maximilian Radoy <maximilian.radoy@uni-paderborn.de>
2025-11-07 14:26:25 +0100

    Change parameter to terminate successfull connections

--- Framework/src/main/java/de/rub/nds/x509anvil/framework/verifier/adapter/TlsServerAuthVerifierAdapterDocker.java
+++ Framework/src/main/java/de/rub/nds/x509anvil/framework/verifier/adapter/TlsServerAuthVerifierAdapterDocker.java
@@ -78,7 +78,7 @@ public class TlsServerAuthVerifierAdapterDocker extends TlsServerAuthVerifierAda
                             .additionalParameters(supplementStartCommand(TlsImplementationType.fromString(config.getImage())));;
             if(TlsImplementationType.fromString(config.getImage()) == TlsImplementationType.NSS) {
                 NSSPkcs12Util.execSetup();
-                builder.certificatePath("sql:/x509-anv-resources/nss_db/").additionalParameters("-R X509-Anvil-CA -q");
+                builder.certificatePath("sql:/x509-anv-resources/nss_db/").additionalParameters("-R X509-Anvil-CA -Q");
             }
 
             tlsClientInstance = builder.build();
