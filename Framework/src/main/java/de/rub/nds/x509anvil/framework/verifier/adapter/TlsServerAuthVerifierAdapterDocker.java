c1b60227339e49f51eef2fc2a89cf7993f114f75
Maximilian Radoy <maximilian.radoy@uni-paderborn.de>
2025-11-07 14:16:23 +0100

    Add ping-only parameter to terminate successfull connections

--- Framework/src/main/java/de/rub/nds/x509anvil/framework/verifier/adapter/TlsServerAuthVerifierAdapterDocker.java
+++ Framework/src/main/java/de/rub/nds/x509anvil/framework/verifier/adapter/TlsServerAuthVerifierAdapterDocker.java
@@ -78,7 +78,7 @@ public class TlsServerAuthVerifierAdapterDocker extends TlsServerAuthVerifierAda
                             .additionalParameters(supplementStartCommand(TlsImplementationType.fromString(config.getImage())));;
             if(TlsImplementationType.fromString(config.getImage()) == TlsImplementationType.NSS) {
                 NSSPkcs12Util.execSetup();
-                builder.certificatePath("sql:/x509-anv-resources/nss_db/").additionalParameters("-R X509-Anvil-CA");
+                builder.certificatePath("sql:/x509-anv-resources/nss_db/").additionalParameters("-R X509-Anvil-CA -q");
             }
 
             tlsClientInstance = builder.build();
