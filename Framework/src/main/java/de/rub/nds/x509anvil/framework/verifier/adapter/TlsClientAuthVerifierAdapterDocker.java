71b1ed1be7be5e9194a5549c811428d527f73187
Felix Lange <felix.lange@uni-paderborn.de>
2025-11-28 13:52:16 +0100

    feat: remove docker restart

--- Framework/src/main/java/de/rub/nds/x509anvil/framework/verifier/adapter/TlsClientAuthVerifierAdapterDocker.java
+++ Framework/src/main/java/de/rub/nds/x509anvil/framework/verifier/adapter/TlsClientAuthVerifierAdapterDocker.java
@@ -25,7 +25,6 @@ import de.rub.nds.x509anvil.framework.verifier.TlsAuthVerifierAdapterConfigDocke
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 import de.rub.nds.x509anvil.framework.verifier.adapter.util.NSSPkcs12Util;
 import de.rub.nds.x509anvil.framework.x509.config.X509Util;
@@ -59,19 +58,10 @@ public class TlsClientAuthVerifierAdapterDocker extends TlsClientAuthVerifierAda
         DockerTlsServerInstance instance = spinUpServer(config);
         return new TlsClientAuthVerifierAdapterDocker(instance, config.getImage());
     }
-    private static int i = 0;
     private static DockerTlsServerInstance spinUpServer(TlsAuthVerifierAdapterConfigDocker config) {
         String key = config.getImage() + ":" + config.getVersion();
         if (tlsServerInstances.containsKey(key)) {
-            if(i++%50 == 0) {
-                try {
-                    tlsServerInstances.get(key).stop();
-                    Thread.sleep(50);
-                } catch (Exception ignored) {
-            }
-            } else  {
-                return tlsServerInstances.get(key);
-            }
+            return tlsServerInstances.get(key);
         }
         TlsImplementationType implementationType =
                 TlsImplementationType.fromString(config.getImage());
