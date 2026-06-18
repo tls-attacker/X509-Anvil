55fc4db338df678c0aa3d24fe0a8b5c8ff8ee0f8
Maximilian Radoy <maximilian.radoy@uni-paderborn.de>
2026-06-18 11:28:00 +0200

    Add playwright adapter

---
+++ Framework/src/main/java/de/rub/nds/x509anvil/framework/verifier/PlaywrightConfig.java
@@ -0,0 +1,25 @@
+/*
+ * X.509-Anvil - A Compliancy Evaluation Tool for X.509 Certificates.
+ *
+ * Copyright 2014-2025 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package de.rub.nds.x509anvil.framework.verifier;
+
+import java.util.Random;
+
+public class PlaywrightConfig extends TlsAuthVerifierAdapterConfig {
+
+    private String browser;
+
+    public PlaywrightConfig(String browser) {
+        super("localhost", new Random().nextInt(10000,65535));
+        this.browser = browser;
+    }
+
+    public String getBrowser() {
+        return browser;
+    }
+}
---
+++ Framework/src/main/java/de/rub/nds/x509anvil/framework/verifier/adapter/PlaywrightAdapter.java
@@ -0,0 +1,37 @@
+package de.rub.nds.x509anvil.framework.verifier.adapter;
+
+import de.rub.nds.x509anvil.framework.verifier.PlaywrightConfig;
+import de.rub.nds.x509anvil.framework.verifier.TlsAuthVerifierAdapterConfig;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class PlaywrightAdapter extends TlsServerAuthVerifierAdapter {
+    private URL target;
+    public PlaywrightAdapter(String browser, String hostname, int port) {
+        super(hostname, port);
+        try {
+            this.target = new URL("https://localhost:3000/"+browser+"?"+port);
+            System.out.println("OPERNING CONECTION");
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static PlaywrightAdapter fromConfig(PlaywrightConfig config) {
+            System.out.println("OPERNING CasdONECTION");
+        return new PlaywrightAdapter(config.getBrowser(), config.getHostname(), config.getPort());
+    }
+
+    @Override
+    public void runCommandInBackground() {
+        try {
+            System.out.println("OPERNING CONECTION");
+            target.openConnection();
+        } catch (IOException e) {
+            //
+            e.printStackTrace();
+        }
+    }
+}
