bfd0aee07d4a7e843be2b7162af1675864247a4f
dds173 <203660498+dds173@users.noreply.github.com>
2026-09-04 16:47:55 +0200

    Add RFC 3279 profile and RsaAlgorithmParametersTests

---
+++ X509-Testsuite/src/main/java/de/rub/nds/x509anvil/suite/tests/basicfields/subjectpkinfo/algorithm/RsaAlgorithmParametersTests.java
@@ -0,0 +1,52 @@
+/*
+ * X.509-Anvil - A Compliancy Evaluation Tool for X.509 Certificates.
+ *
+ * Copyright 2014-2025 Ruhr University Bochum, Paderborn University, Technology Innovation Institute, and Hackmanit GmbH
+ *
+ * Licensed under Apache License, Version 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package de.rub.nds.x509anvil.suite.tests.basicfields.subjectpkinfo.algorithm;
+
+import de.rub.nds.anvilcore.annotation.AnvilTest;
+import de.rub.nds.x509anvil.framework.annotation.ChainLength;
+import de.rub.nds.x509anvil.framework.anvil.X509AnvilTest;
+import de.rub.nds.x509anvil.framework.anvil.X509VerifierRunner;
+import de.rub.nds.x509anvil.framework.verifier.VerifierException;
+import de.rub.nds.x509anvil.framework.x509.generator.CertificateGeneratorException;
+import de.rub.nds.x509anvil.framework.x509.generator.modifier.X509CertificateConfigModifier;
+import de.rub.nds.x509attacker.constants.X509PublicKeyType;
+import de.rub.nds.x509attacker.x509.model.publickey.parameters.X509DhParameters;
+
+import org.junit.jupiter.api.TestInfo;
+
+public class RsaAlgorithmParametersTests extends X509AnvilTest {
+
+    @ChainLength(minLength = 2)
+    @AnvilTest(id = "basic-subjectpki-algorithm-a65cdbb3b8")
+    public void subjectPublicKeyInfoRsaAlgorithmParametersNotNullEntity(X509VerifierRunner testRunner, TestInfo testInfo)
+            throws VerifierException, CertificateGeneratorException {
+        assertInvalid(
+                testRunner,
+                true,
+                (X509CertificateConfigModifier)
+                        config -> {
+                            config.setPublicKeyType(X509PublicKeyType.RSA);
+                            config.setPublicKeyParameters(new X509DhParameters("dhParameters"));
+                        }, testInfo);
+    }
+
+    @ChainLength(minLength = 3)
+    @AnvilTest(id = "basic-subjectpki-algorithm-21f27b8d32")
+    public void subjectPublicKeyInfoRsaAlgorithmParametersNotNullIntermediate(X509VerifierRunner testRunner, TestInfo testInfo)
+            throws VerifierException, CertificateGeneratorException {
+        assertInvalid(
+                testRunner,
+                false,
+                (X509CertificateConfigModifier)
+                        config -> {
+                            config.setPublicKeyType(X509PublicKeyType.RSA);
+                            config.setPublicKeyParameters(new X509DhParameters("dhParameters"));
+                        }, testInfo);
+    }
+}
